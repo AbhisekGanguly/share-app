ccd310eadb128385a80032b31e020e60c8dd76ed
Abhisek Ganguly <43989559+AbhisekGanguly@users.noreply.github.com>
2022-07-15 14:50:19 +0530

    modified server.js

--- server.js
+++ server.js
@@ -11,10 +11,16 @@ const connectDB = require('./config/db');
 connectDB();
 
 // CORs policy
-const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 }
-app.use(cors(corsOptions));
+
+app.use(allowCrossDomain);
+
 
 
 //connect to the frontend
